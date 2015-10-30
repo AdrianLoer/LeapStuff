d18d342aa4bdaab1bb5971474796a4d6b9727146
Adrian Loer <adrian.loer@gmail.com>
2015-10-30 02:33:22 +0100

    angle calculated correctly

--- libs/mathUtility.js
+++ libs/mathUtility.js
@@ -17,10 +17,7 @@ var mUtil = {
 
 	// takes the perpendicular vector to the plane and the line vector
 	vecAngleLinePlane: function(v1, v2) {	
-		console.log("v1", v1);
-		console.log("v2", v2);
-		Leap.vec3.multiply(this.tmpMultipliedVector, v1, v2);
-		return Math.asin(Math.abs(Leap.vec3.dot(a,b)) / (Leap.vec3.length(v1) * Leap.vec3.length(v2))) * this.TO_DEG;
+		return Math.asin(Math.abs(Leap.vec3.dot(v1,v2)) / (Leap.vec3.length(v1) * Leap.vec3.length(v2))) * this.TO_DEG;
 	}
 
 }
