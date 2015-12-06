a64f28fb5634f6251552e7eda344028e623526f3
Adrian Loer <adrian.loer@gmail.com>
2015-12-06 18:52:49 +0100

    ws server stores gesture files

--- reactiveStuff/server.js
+++ reactiveStuff/server.js
@@ -1,17 +1,37 @@
 var http = require('http').Server();
 var io = require('socket.io')(http);
+var fs = require('fs');
 
-io.on('connection', function(socket){
-  console.log('a user connected');
 
-  // receives a command from the frontend and deceides how to handle this
-  // in most cases it wil 
+io.on('connection', function(socket) {
+  console.log('connection...');
+
   socket.on('msg', function (msg) {
   	console.log("msg: ", msg);
+  	writeGestureToFs(msg);
   });
 
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:3000...');
 });
+
+
+
+function writeGestureToFs(gestureInfo) {
+
+	var dir = './gestureData/' + gestureInfo.gestureName;
+	if (!fs.existsSync(dir)){
+	    fs.mkdirSync(dir);
+	}
+
+	fs.writeFile(dir + "/" + (new Date().getTime()) + ".json", JSON.stringify(gestureInfo), function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	    console.log("The file was saved!");
+	}); 
+
+}
